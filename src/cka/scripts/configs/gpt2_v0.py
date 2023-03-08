c3ae23cd17134511f8645e6899b2f6da59099413
Daniel Furman <dryanfurman@gmail.com>
2023-03-08 00:58:30 -0800

    test

--- src/cka/scripts/configs/gpt2_v0.py
+++ src/cka/scripts/configs/gpt2_v0.py
@@ -3,7 +3,7 @@ config = {
         "distilgpt2",  # 82M params
         "gpt2",  # 124M params
         "gpt2-medium",  # 355M params
-        # "gpt2-large",  # 774M params
+        "gpt2-large",  # 774M params
         # "gpt2-xl",  # 1.5B params
     ],
     "input_information": {
