d14ca98832c400ec6cf5df9acdd9a32d66c15505
Daniel Furman <danielfurman@Daniels-MacBook-Pro.local>
2023-03-07 19:53:42 -0800

    return error fixed in main.py

--- src/cka/scripts/run_cka.py
+++ src/cka/scripts/run_cka.py
@@ -9,7 +9,12 @@ from compare_models import compare_models
 def main(config):
 
     set_seed(42)
-    compare_models(config["models"], config["input_information"])
+
+    score_dict_full, score_dict_succinct = compare_models(
+        config["models"], config["input_information"]
+    )
+
+    return score_dict_full, score_dict_succinct
 
 
 if __name__ == "__main__":
@@ -17,4 +22,6 @@ if __name__ == "__main__":
     parser.add_argument("configs", type=str, help="Config file to set up run pipeline")
     args = parser.parse_args()
     config = importlib.import_module(args.configs).config
-    main(config)
+    score_dict_full, score_dict_succinct = main(config)
+    print(f"\nScore dict full:\n{score_dict_full}")
+    print(f"\nScore dict succinct:\n{score_dict_succinct}")
