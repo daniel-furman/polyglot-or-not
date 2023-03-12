04e1e3150853c190c6d3e5885541c8602d3309ae
Daniel Furman <dryanfurman@gmail.com>
2023-03-12 12:06:44 -0700

    changing llama rome first test to 7b model in config

--- src/cka_scripts/configs/rome_full/llama_rome_full.py
+++ src/cka_scripts/configs/rome_full/llama_rome_full.py
@@ -5,7 +5,7 @@ with open("../../data/rome_counterfact_input_information.json", "r") as f:
 
 config = {
     "models": [
-        "/content/drive/MyDrive/Colab Files/llama/LLaMA/int8/llama-13b/",
+        "/content/drive/MyDrive/Colab Files/llama/LLaMA/int8/llama-7b/",
     ],
     "input_information": input_info,
     "verbosity": False,
