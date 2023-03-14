5bc95ff63a9757018386aa1eefd715a3dc57c9a5
Daniel Furman <dryanfurman@gmail.com>
2023-03-14 09:07:38 -0700

    a few more configs for full benchmark added

--- src/cka_scripts/configs/calibragpt_benchmark/roberta_calibragpt.py
+++ src/cka_scripts/configs/calibragpt_benchmark/roberta_calibragpt.py
@@ -7,9 +7,9 @@ config = {
     "models": [
         "distilroberta-base",  # 82M params
         "roberta-base",  # 125M params
-        "xlm-roberta-base",  # 125M params
+        # "xlm-roberta-base",  # 125M params
         "roberta-large",  # 354M params
-        "xlm-roberta-large",  # 354M params
+        # "xlm-roberta-large",  # 354M params
     ],
     "input_information": input_info,
     "verbosity": False,
