915ccddfad9f4678080ecf73ead92860074279ec
Daniel Furman <dryanfurman@gmail.com>
2023-03-13 21:12:54 -0700

    new full benchmark configs added

---
+++ src/cka_scripts/configs/calibragpt_benchmark/llama_calibragpt.py
@@ -0,0 +1,12 @@
+import json
+
+with open("../../data/calibragpt_full_input_information.json", "r") as f:
+    input_info = json.load(f)
+
+config = {
+    "models": [
+        "/content/drive/MyDrive/Colab Files/llama/LLaMA/int8/llama-7b/",
+    ],
+    "input_information": input_info,
+    "verbosity": False,
+}
