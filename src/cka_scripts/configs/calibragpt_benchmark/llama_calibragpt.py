2d0a042ea6c7492f52d8394ff5a33eec6ef2975b
Daniel Furman <dryanfurman@gmail.com>
2023-03-17 10:54:30 -0700

    llama config updated

--- src/cka_scripts/configs/calibragpt_benchmark/llama_calibragpt.py
+++ src/cka_scripts/configs/calibragpt_benchmark/llama_calibragpt.py
@@ -5,7 +5,7 @@ with open("../../data/calibragpt_full_input_information.json", "r") as f:
 
 config = {
     "models": [
-        "/content/drive/MyDrive/Colab Files/llama/LLaMA/int8/llama-7b/",
+        "/content/drive/MyDrive/Colab Files/llama/LLaMA/int8/llama-30b/",
     ],
     "input_information": input_info,
     "verbosity": False,
