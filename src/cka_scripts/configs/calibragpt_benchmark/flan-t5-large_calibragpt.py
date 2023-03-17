5297b11dc149cd48ed5c6969784f0cf23bd5d319
Daniel Furman <dryanfurman@gmail.com>
2023-03-16 18:38:54 -0700

    flan configs updated for run

---
+++ src/cka_scripts/configs/calibragpt_benchmark/flan-t5-large_calibragpt.py
@@ -0,0 +1,16 @@
+import json
+
+with open("../../data/calibragpt_full_input_information.json", "r") as f:
+    input_info = json.load(f)
+
+config = {
+    "models": [
+        # "google/flan-t5-small",  # 80M params
+        # "google/flan-t5-base",  # 250M params
+        "google/flan-t5-large",  # 780M params
+        # "google/flan-t5-xl",  # 3B params
+        # "google/flan-t5-xxl",  # 11B params
+    ],
+    "input_information": input_info,
+    "verbosity": False,
+}
--- src/cka_scripts/configs/calibragpt_benchmark/flan-t5-xl_calibragpt.py
+++ src/cka_scripts/configs/calibragpt_benchmark/flan-t5-xl_calibragpt.py
@@ -8,7 +8,7 @@ config = {
         # "google/flan-t5-small",  # 80M params
         # "google/flan-t5-base",  # 250M params
         # "google/flan-t5-large",  # 780M params
-        # "google/flan-t5-xl",  # 3B params
+        "google/flan-t5-xl",  # 3B params
         # "google/flan-t5-xxl",  # 11B params
     ],
     "input_information": input_info,
