abc8bf6ce7b31669c4917428e3f6a828e1597052
Daniel Furman <dryanfurman@gmail.com>
2023-03-18 15:21:25 -0700

    test on 3b bloom in the config instead of 7b

--- src/cka_scripts/configs/calibragpt_benchmark/blooms_calibragpt.py
+++ src/cka_scripts/configs/calibragpt_benchmark/blooms_calibragpt.py
@@ -4,7 +4,7 @@ with open("../../data/calibragpt_full_input_information.json", "r") as f:
     input_info = json.load(f)
 
 config = {
-    "models": ["bigscience/bloom-7b1", "bigscience/bloom-3b"],
+    "models": ["bigscience/bloom-3b"],
     "input_information": input_info,
     "verbosity": False,
 }
