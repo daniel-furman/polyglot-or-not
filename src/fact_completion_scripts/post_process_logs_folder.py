9119aaa042a33de13ce3d418436bcad5a7b0cf2f
daniel-furman <dryanfurman@gmail.com>
2023-04-23 08:44:01 -0700

    new t5/mt5 results added

--- src/fact_completion_scripts/post_process_logs_folder.py
+++ src/fact_completion_scripts/post_process_logs_folder.py
@@ -3,7 +3,7 @@ Process fact-completion logs to generate bootstrap estimates and error analysis 
 
 Example usage:
 python post_process_logs_folder.py \
-    --folder ../../data/result_logs/xlm-roberta \
+    --folder ../../data/result_logs/llama \
     --write_errors True
 """
 
