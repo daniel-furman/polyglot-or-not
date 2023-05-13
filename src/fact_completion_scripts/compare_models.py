07395c8cbd7c50ce7a920832fadf56cb8094dba9
daniel-furman <dryanfurman@gmail.com>
2023-05-13 16:55:35 -0700

    llama precision experiments

--- src/fact_completion_scripts/compare_models.py
+++ src/fact_completion_scripts/compare_models.py
@@ -90,8 +90,8 @@ def get_model_and_tokenizer(model_name):
         ), transformers.LLaMAForCausalLM.from_pretrained(
             model_name,
             device_map="auto",
+            load_in_8bit=True,
             torch_dtype=torch.float16,
-            # , load_in_8bit=True, torch_dtype=torch.float16
         )
 
 
