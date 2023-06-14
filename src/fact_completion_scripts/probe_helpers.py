7940baa87be6a1508a256a95bfc443e454e1a42f
daniel-furman <dryanfurman@gmail.com>
2023-06-13 19:03:50 -0700

    patching...

--- src/fact_completion_scripts/probe_helpers.py
+++ src/fact_completion_scripts/probe_helpers.py
@@ -133,13 +133,13 @@ def probe_falcon(model, tokenizer, target_id, context, verbose=False):
         output_scores=True,
         return_dict=True,
         return_dict_in_generate=True,
-        max_new_tokens=2,
+        max_new_tokens=1,
     )
 
     # find the left-most non-sepecial token, save itr of this token to grab
     # correct logit scores array
     sequences = outputs["sequences"][0].tolist()
-    for i in range(2):
+    for i in range(1):
         logits = outputs["scores"][i]
         probs = softmax(logits, dim=-1)
         probs = probs.detach().cpu().numpy()
@@ -358,21 +358,42 @@ def probe_llama(model, tokenizer, target_id, context, verbose=False):
     # tokenize context
     input_ids = tokenizer(
         context,
+        padding="longest",
+        max_length=2048,
+        truncation=True,
         return_tensors="pt",
-    ).input_ids.to(device)
-
-    # grab value
-    target_scalar = target_id.detach().cpu().numpy()
-
+    ).input_ids
     # use model to solicit a prediction
-    outputs = model(input_ids=input_ids, output_hidden_states=True, return_dict=True)
+    outputs = model.generate(
+        input_ids=input_ids.to(device),
+        output_scores=True,
+        return_dict=True,
+        return_dict_in_generate=True,
+        max_new_tokens=1,
+    )
 
-    # every token in the model's vocab gets a representative prediction from the model
-    logits = outputs["logits"][0, -1]
+    # find the left-most non-sepecial token, save itr of this token to grab
+    # correct logit scores array
+    sequences = outputs["sequences"][0].tolist()
+    for i in range(1):
+        logits = outputs["scores"][i]
+        probs = softmax(logits, dim=-1)
+        probs = probs.detach().cpu().numpy()
+        if tokenizer.decode([np.argmax(probs)]) not in [
+            "<|endoftext|>",
+            "<|padding|>",
+            "",
+            " ",
+        ]:
+            save_itr = i
+            break
+    # grab its logits
+    logits = outputs["scores"][save_itr]
     # convert our prediction scores to a probability distribution with softmax
     probs = softmax(logits, dim=-1)
-
-    probs = list(probs.detach().cpu().numpy())
+    probs = probs.detach().cpu().numpy()
+    # grab the full decoded output for verbose:
+    decoded_output = tokenizer.decode(sequences)
 
     if verbose:
         print(f"\n\tcontext... {context}")
@@ -380,18 +401,7 @@ def probe_llama(model, tokenizer, target_id, context, verbose=False):
         print(f"\tdecoded tokenized_context... {tokenizer.decode(input_ids[0])}")
         print(f"\tdecoded target id... {tokenizer.decode([target_id.item()])}")
         print(
-            f"\tmost probable prediction id decoded... {tokenizer.decode([np.argmax(probs)])}\n"
+            f"\tmost probable prediction id decoded... {tokenizer.decode([np.argmax(probs)])}"
         )
-
-    # double check weird-ness before accessing prob
-    if len(probs) < target_id:
-        return None
-
-    # return the likelihood that our stipulated target would follow the context,
-    # according to the model
-    try:
-        return np.take(probs, [target_scalar])[0]
-
-    except IndexError:
-        print("target index not in model vocabulary scope; raising IndexError")
-        return None
+        print(f"\tdecoded full text generate output... {decoded_output}\n")
+    return probs[0][target_id.item()]
